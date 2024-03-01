0a874004964febc335c00080de21ad09c8230a6a
ilyas@plandstudios.com <saylikhan97@gmail.com>
2024-03-01 19:00:07 +0500

    Second Commit

--- js/jscript.js
+++ js/jscript.js
@@ -1,1 +1,21 @@
 <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
+
+$(document).ready(function () {
+
+    $('#navtoggle').on('click', function () {
+        $('#sidebar').addClass('d-flex')
+        $('#sidebar').removeClass('d-none')
+        $('#sidebar').addClass('d-flex')
+
+    })
+
+    $('#toggle-out').on('click', function () {
+        $('#sidebar').removeClass('d-flex')
+        $('#sidebar').addClass('d-none')
+        $('#sidebar').removeClass('d-flex')
+    })
+
+
+});
+
+
